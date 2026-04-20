b8d2a78350f147556697f83ea4e3e455fd419e4c
Erfan Sadigh Nejati <erfansn.es@gmail.com>
2026-04-20 13:01:48 +0330

    Move Url data type to its file

---
+++ app/src/main/java/ir/nooshdaroo/Url.kt
@@ -0,0 +1,30 @@
+package ir.nooshdaroo
+
+import android.util.Patterns
+
+@JvmInline
+value class Url(val address: String) {
+
+    init {
+        require(address.contains("localhost") || Patterns.WEB_URL.matcher(address).matches()) { "Invalid url address $address" }
+    }
+
+    val path: String?
+        get() {
+            var count = 3
+            var cIndex = 0
+            for ((index, ch) in address.withIndex()) {
+                if (ch == '/') {
+                    cIndex = index
+                    count--
+                }
+                if (count == 0) break
+            }
+
+            return if (count != 0 || cIndex == address.lastIndex) {
+                null
+            } else {
+                address.substring(cIndex).substringBeforeLast("#").substringBeforeLast("?")
+            }
+        }
+}

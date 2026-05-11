2c1841c1189ccc5602038ef0ad1e81bd8d76ab40
Erfan Sadigh Nejati <erfansn.es@gmail.com>
2026-05-11 19:11:48 +0330

    Specify type explicitly

--- app/src/test/java/ir/nooshdaroo/Correspondence.kt
+++ app/src/test/java/ir/nooshdaroo/Correspondence.kt
@@ -3,7 +3,7 @@ package ir.nooshdaroo
 import com.google.common.truth.Correspondence
 import ir.nooshdaroo.data.model.Category
 
-val CategoryCorrespondence = Correspondence.from<Category, Category>(
-    { actual, expected -> actual.url?.path == expected.url?.path && actual.title == expected.title },
+val CategoryCorrespondence: Correspondence<Category, Category> = Correspondence.from<Category, Category>(
+    { actual, expected -> actual.url.path == expected.url.path && actual.title == expected.title },
     "contains"
-)!!
+)

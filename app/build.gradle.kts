19593e176e86651cd62e701e43a35f495714401e
Erfan Sadigh Nejati <erfansn.es@gmail.com>
2026-05-14 17:28:41 +0330

    Enable code minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -54,6 +54,10 @@ android {
 
     androidResources {
         generateLocaleConfig = true
+    }
+
+    lint {
+        checkReleaseBuilds = false
     }
 }
 
